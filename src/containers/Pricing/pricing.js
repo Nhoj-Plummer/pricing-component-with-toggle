5e2b2d61abc0158a5740f4082e3aa202f670c264
Nhoj <nhojplummer@yahoo.com>
2023-10-30 13:54:01 -0400

    Setting up process for challenge

--- src/containers/Pricing/pricing.js
+++ src/containers/Pricing/pricing.js
@@ -1,8 +1,13 @@
 import React from "react"
+import {Card} from "../../components/componentindex"
 import "./Pricing.css"
 
 const Pricing = () => {
-  return <div className="pricing-container"></div>
+  return (
+    <div className="pricing-container">
+      <Card />
+    </div>
+  )
 }
 
 export default Pricing

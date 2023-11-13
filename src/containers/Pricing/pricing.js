8385e24a0e45de7cb4dc55e49f9930636771431e
Nhoj <nhojplummer@yahoo.com>
2023-11-13 10:52:15 -0500

    trying to figure out liast bit of info for hover effect

--- src/containers/Pricing/pricing.js
+++ src/containers/Pricing/pricing.js
@@ -36,7 +36,7 @@ const Pricing = () => {
     {id: 1, title: "Basic", price: "199.99", storage: "500 GB Storage", users: "2 Users Allowed", sendGB: "Send up to 3 GB", textColor: {color: "var(--color-grayish-blue)"}},
     {
       id: 2,
-      style: {background: "var(--color-gradient)", color: "var( --color-very-light-grayish-blue)", position: width > 540 ? "relative" : "", height: width > 540 ? "32rem" : "", boxShadow: width > 540 ? "none" : "" },
+      style: {background: "var(--color-gradient)", color: "var( --color-very-light-grayish-blue)", position: width > 540 ? "relative" : "", height: width > 540 ? "27rem" : "", boxShadow: width > 540 ? "none" : ""},
       title: "Professional",
       price: "249.99",
       storage: "1 TB Storage",
@@ -67,7 +67,6 @@ const Pricing = () => {
             console.log("option", option);
             return <Card id={option.id} style={option.style} title={option.title} price={option.price} storage={option.storage} users={option.users} sendGB={option.sendGB} textColor={option.textColor} buttonColor={option.buttonColor} />;
           })}
-          {/* <Card title="Annually" /> */}
         </div>
       )}
 

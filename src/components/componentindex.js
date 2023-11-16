9b160d72348c11f645d0e8619cc75d35da8af118
Nhoj <nhojplummer@yahoo.com>
2023-11-16 17:13:47 -0500

    Making a whole new component for the middle card

--- src/components/componentindex.js
+++ src/components/componentindex.js
@@ -1,2 +1,3 @@
-export {default as Card} from "../components/Card/Card"
-export {default as Toggle} from "../components/Toggle/Toggle"
+export {default as Card} from "../components/Card/Card";
+export {default as MiddleCard} from "../components/MiddleCard/MiddleCard";
+export {default as Toggle} from "../components/Toggle/Toggle";

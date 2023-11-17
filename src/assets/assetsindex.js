cf02e7f813cddf99da6552eb1246ba437c778fc7
Nhoj <nhojplummer@yahoo.com>
2023-11-17 11:37:35 -0500

    Added background now for cleanup

--- src/assets/assetsindex.js
+++ src/assets/assetsindex.js
@@ -1,2 +1,2 @@
-export {default as Bottom} from "../assets/bg-bottom.svg"
-export {default as Top} from "../assets/bg-top.svg"
+export {default as Top} from "../assets/bg-top.svg";
+export {default as Bottom} from "../assets/bg-bottom.svg";

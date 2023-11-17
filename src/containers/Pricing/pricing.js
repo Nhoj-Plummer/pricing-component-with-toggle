fd20bfc1434ec82c6243376c0277e12f0ffc140f
Nhoj <nhojplummer@yahoo.com>
2023-11-17 12:05:16 -0500

    Fixed a merging conflict with files

--- src/containers/Pricing/pricing.js
+++ src/containers/Pricing/pricing.js
@@ -4,20 +4,6 @@ import "./Pricing.css";
 
 const Pricing = () => {
   const [price, setPrice] = useState();
-  // Data used for map function
-  // const annualData = [
-  //   {id: "O1", title: "Basic", price: "199.99", storage: "500 GB Storage", users: "2 Users Allowed", sendGB: "Send up to 3 GB", textColor: {color: "var(--color-grayish-blue)"}},
-  //   {
-  //     id: "O2",
-  //     style: {background: "var(--color-gradient)", color: "var( --color-very-light-grayish-blue)"},
-  //     title: "Professional",
-  //     price: "249.99",
-  //     storage: "1 TB Storage",
-  //     users: "5 Users Allowed",
-  //     sendGB: "Send up to 10 GB"
-  //   },
-  //   {id: "O3", title: "Master", price: "399.99", storage: "2 TB Storage", users: "10 Users Allowed", sendGB: "Send up to 20 GB", textColor: {color: "var(--color-grayish-blue)"}}
-  // ];
 
   return (
     <div className="pricing-container">

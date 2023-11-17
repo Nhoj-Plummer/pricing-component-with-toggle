09c5e5ae937292b97016e97fb22404ac6a1ff1fa
Nhoj <nhojplummer@yahoo.com>
2023-11-17 11:14:40 -0500

    Challenge nearly completed

--- src/containers/Pricing/pricing.js
+++ src/containers/Pricing/pricing.js
@@ -36,7 +36,7 @@ const Pricing = () => {
       {!price && (
         <div className="pricing-container_prices annually">
           <Card title="Basic" price="199.99" storage="500 GB Storage" users="2 Users Allowed" sendGB="Send up to 3 GB" />
-          <MiddleCard title="Basic" price="199.99" storage="500 GB Storage" users="2 Users Allowed" sendGB="Send up to 3 GB" />
+          <MiddleCard title="Professional" price="249.99" storage="1 TB Storage" users="5 Users Allowed" sendGB="Send up to 10 GB" />
           <Card title="Master" price="399.99" storage="2 TB storage" users="10 Users Allowed" sendGB="Send up to 20 GB" />
         </div>
       )}
@@ -44,7 +44,9 @@ const Pricing = () => {
       {/* Monthly Options Mapping/Conditioning */}
       {price && (
         <div className="pricing-container_prices mnothly">
-          <Card title="Monthly" />
+          <Card title="Basic" price="19.99" storage="500 GB Storage" users="2 Users Allowed" sendGB="Send up to 3 GB" />
+          <MiddleCard title="Professional" price="24.99" storage="500 GB Storage" users="2 Users Allowed" sendGB="Send up to 3 GB" />
+          <Card title="Master" price="39.99" storage="2 TB storage" users="10 Users Allowed" sendGB="Send up to 20 GB" />
         </div>
       )}
     </div>
